9cdcbde5459bd0b5d259f2a127cebbcc9640fd90
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-06-09 11:33:18 +0100

    drop port

--- linux/step01_ubuntu1604_ice_deps.sh
+++ linux/step01_ubuntu1604_ice_deps.sh
@@ -7,7 +7,7 @@ if [ "$ICEVER" = "ice36" ]; then
 
     #start-recommended
     apt-get -y install software-properties-common
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 5E6DA83306132997
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 5E6DA83306132997
     apt-add-repository "deb http://zeroc.com/download/apt/ubuntu`lsb_release -rs` stable main"
     apt-get update
     apt-get -y install zeroc-ice-all-runtime
