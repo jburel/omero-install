7637ae6db8788e56f0674d9db259426cc51913a0
Jean-Marie Burel <j.burel@dundee.ac.uk>
2024-11-20 20:20:52 +0000

    Install 5.6.14rc1

--- linux/step04_all_omero_install.sh
+++ linux/step04_all_omero_install.sh
@@ -10,7 +10,7 @@ set -eux
 if [ $OMEROVER == "latest" ]; then
 	#start-release-ice36
 	cd /opt/omero/server
-	SERVER=https://downloads.openmicroscopy.org/omero/5.6/server-ice36.zip
+	SERVER=https://github.com/jburel/openmicroscopy/releases/download/v5.6.14rc1/openmicroscopy-5.6.14rc1-ice36.zip
 	wget -q $SERVER -O OMERO.server-ice36.zip
 	unzip -q OMERO.server*
 	#end-release-ice36
